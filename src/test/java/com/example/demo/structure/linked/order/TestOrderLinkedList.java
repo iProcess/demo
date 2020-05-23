b2ec3f0874474aa9ba39bd2e434dc7d6e8a7678b
shijincheng3 <shijincheng@jd.com>
2020-05-23 12:23:45 +0800

    OrderLinkedList

---
+++ src/test/java/com/example/demo/structure/linked/order/TestOrderLinkedList.java
@@ -0,0 +1,23 @@
+package com.example.demo.structure.linked.order;
+
+public class TestOrderLinkedList {
+
+    public static void main(String[] args) {
+//        OrderLinkedList linkedList1 = new OrderLinkedList();
+//        linkedList1.addHead(3);
+//        linkedList1.addHead(5);
+//        linkedList1.addHead(4);
+//        linkedList1.addHead(10);
+//        linkedList1.addHead(2);
+//        linkedList1.disply();
+
+        OrderLinkedList linkedList2 = new OrderLinkedList();
+        linkedList2.addHead1(3);
+        linkedList2.addHead1(5);
+        linkedList2.addHead1(4);
+        linkedList2.addHead1(10);
+        linkedList2.addHead1(2);
+        linkedList2.disply();
+
+    }
+}
