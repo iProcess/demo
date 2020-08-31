a159134151447bce869180cf8d480ff642917ded
“iProcess” <26076408@qq.com>
2020-08-31 23:20:15 +0800

    interview

---
+++ src/test/java/com/example/demo/interview/Interview.java
@@ -0,0 +1,18 @@
+package com.example.demo.interview;
+
+/**
+ * [2,4,6,8]
+ * [8,2,4,6]
+ */
+public class Interview {
+
+    public int search(int[] array, int target){
+        if(){
+
+        }
+
+
+        return -1;
+    }
+
+}
