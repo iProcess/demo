a0b4f5aa898b511ab1690021c0b9582077eee4fd
shijincheng3 <shijincheng@jd.com>
2020-11-04 09:41:53 +0800

    快速排序

--- src/test/java/com/example/demo/test/ali/gd/SelectSort.java
+++ src/test/java/com/example/demo/test/ali/gd/SelectSort.java
@@ -1,12 +1,22 @@
 package com.example.demo.test.ali.gd;
 
+/**
+ * 可能有误
+ * 第二层循环：
+ * 1、下标应是j = 1；
+ * 2、边界值j应是j <= array.length - i。
+ * 这样的后果是：第一个元素会和自己做比较，即：array[0]和array[0]做比较，且最后一个元素不会参与比较大小。
+ * 对于3 6 7 9 0来说，因最后一位是0，没有比较就进行了交互到第一位，所以看起来是正确的，
+ * 如换成3 6 7 9 10，就不正确了
+ */
 public class SelectSort {
 
     //3 6 7 9 0
     public static void selectSort(int[] array){
         for(int i = 1; i < array.length; i++){
             int index = 0;
-            for(int j = 0; j < array.length - i; j++){
+            //for(int j = 0; j < array.length - i; j++){
+            for(int j = 1; j <= array.length - i; j++){//修改后
                 if(array[j] > array[index]){
                     index = j;
                 }
@@ -22,7 +32,7 @@ public class SelectSort {
     }
 
     public static void main(String[] args) {
-        int[] array = {3, 6, 7, 9, 0};
+        int[] array = {3, 6, 7, 9, 10};
         selectSort(array);
         for(int i : array){
             System.out.print(i + " ");
